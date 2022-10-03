d85c97dfbdb8a31a462b83ed48e930d724ea2637
Chito <Javier.martinez632@gmail.com>
2022-10-03 16:29:31 -0700

    updated code

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var passwordOptions = {
-  option1: "Include lowercase?",
-  option2: "Incude uppercase?",
-  option3: "Include numeric characters?",
-  option4: "Include symbolic characters?",
-};
-
-
-
+var generateBtn = document.querySelector("#generate"); 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -20,23 +11,55 @@ function writePassword() {
 
 
 function generatePassword() {
-  var passwordLength = prompt("Enter the number of characters you want in your password. Must greater than 8 but no more than 128."); 
-  var passwordChars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()".split('');
-
-  confirm(passwordOptions.option1);
-  confirm(passwordOptions.option2);
-  confirm(passwordOptions.option3);
-  confirm(passwordOptions.option4);
-
-  for(var i = 0; i <= passwordLength; i++) {
-    var index = Math.floor(Math.random() * passwordChars.length);
-    word += passwordChars.substring(index, index +1);
-
+  var passwordLength = prompt("Enter length of password: 8-128 characters"); 
+  var option1 = confirm("Include lowercase?");
+  var option2 = confirm("Include uppercase?");
+  var option3 = confirm("Include numbers?");
+  var option4 = confirm("Include symbols?");
+  var lowercaseChar = ["abcdefghijklmnopqrstuvwxyz"];
+  var uppercaseChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var numericChar = ["0123456789"];
+  var symbolicChar = ["!@#$%^&*()"];
+  var otherChar = [];
+  var message = "";
+  
+  if(passwordLength <8 || passwordLength > 128) {
+    alert("Must be 8-128 characters long");
+    generatePassword();
+  }
+  
+ if(option1) {
+  otherChar = otherChar.concat(lowercaseChar);
+  } else if(option1 && option2) {
+     otherChar = otherChar.concat(lowercaseChar, uppercaseChar);
+  } else if(option1 && option2 && option3) {
+    otherChar = otherChar.concat(lowercaseChar, uppercaseChar, numericChar);
+  } else if(option1 && option2 && option3 && option4) {
+    otherChar = otherChar.concat(lowercaseChar, uppercaseChar, numericChar, symbolicChar);
+  } else if(option2 && option3 && option4) {
+    otherChar = otherChar.concat(uppercaseChar, numericChar, symbolicChar);
+  } else if(option1 && option2 && option4) {
+    otherChar = otherChar.concat(lowercaseChar, uppercaseChar, symbolicChar);
+  } else if(option1 && option3 && option4) {
+    otherChar = otherChar.concat(lowercaseChar, numericChar, symbolicChar);  
+  } else if(option1 && option3) {
+    otherChar = otherChar.concat(lowercaseChar, numericChar);
+  } else if(option1 && option4) {
+    otherChar = otherChar.concat(lowercaseChar, symbolicChar);
+  } else if(option2 && option3) {
+    otherChar = otherChar.concat(uppercaseChar, numericChar);
+  } else if(option2 && option4) {
+    otherChar = otherChar.concat(uppercaseChar, symbolicChar);
+  } else if(option3 && option4) {
+    otherChar = otherChar.concat(numericChar, symbolicChar);
   };
-  return;
+
+  for(var i = 0; i < otherChar.length; i++) {
+    var random = Math.floor(Math.random() * otherChar.length);
+    message += otherChar[random];
+
+ };
 };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
