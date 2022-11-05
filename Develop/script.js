bf9963a6ef859f928f6149115947176cb1446aee
Chito <Javier.martinez632@gmail.com>
2022-11-05 14:32:43 -0700

    changed js to make character count function correctly

--- Develop/script.js
+++ Develop/script.js
@@ -1,68 +1,44 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate"); 
+var generateBtn = document.querySelector("#generate");  
+// var option1 = confirm("Include lowercase?");
+// var option2 = confirm("Include uppercase?");
+// var option3 = confirm("Include numbers?");
+// var option4 = confirm("Include symbols?");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); 
   
   passwordText.value = password;
-  
-};
 
 
-function generatePassword() {
-  var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-  var lower = lowercaseChar.split("");
-  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var upper = uppercaseChar.split("");
-  var numericChar = "0123456789";
-  var num = numericChar.split("");
-  var symbolicChar = "!@#$%^&*()";
-  var sym = symbolicChar.split("");
-  var otherChar = [];
+  function generatePassword() {
+    var charLower = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+    var charUpper = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
+    var charNum = ["0,1,2,3,4,5,6,7,8,9"];
+    var charSym = ["!,@,#,$,%,^,&,*,(,),[,],{,}"];
+    var otherChar = [];
+    var passwordLength = prompt("Enter length of password: 8-128 characters");
+    var lengthPW = 8 < passwordLength && passwordLength < 128;
+    
+console.log(lengthPW);
 
-  var passwordLength = prompt("Enter length of password: 8-128 characters"); 
-  var option1 = confirm("Include lowercase?");
-  var option2 = confirm("Include uppercase?");
-  var option3 = confirm("Include numbers?");
-  var option4 = confirm("Include symbols?");
-  
-  if(passwordLength <8 || passwordLength > 128) {
-    alert("Must be 8-128 characters long");
-    generatePassword();
-  }
-     
- if(option1) {
-       otherChar.concat(lower);
-  } else if(option1 && option2) {
-       otherChar.concat(lower, upper);
-  } else if(option1 && option2 && option3) {
-       otherChar.concat(lower, upper, num);
-  } else if(option1 && option2 && option3 && option4) {
-       otherChar.concat(lower, upper, num, sym);
-  } else if(option2 && option3 && option4) {
-     otherChar.concat(upper, num, sym);
-  } else if(option1 && option2 && option4) {
-     otherChar.concat(lower, upper, sym);
-  } else if(option1 && option3 && option4) {
-     otherChar.concat(lower, num, sym);  
-  } else if(option1 && option3) {
-     otherChar.concat(lower, num);
-  } else if(option1 && option4) {
-     otherChar.concat(lower, sym);
-  } else if(option2 && option3) {
-     otherChar.concat(upper, num);
-  } else if(option2 && option4) {
-     otherChar.concat(upper, sym);
-  } else if(option3 && option4) {
-     otherChar.concat(num, sym);
+    if (!lengthPW) {
+      alert("Must be 8-128 characters long");
+      generatePassword();
+    } else {}
+       
+   if(option1 || option2 || option3 || option4) {
+     otherChar.concat();
+        
+    for(var i = 0; i < otherChar.concat().length; i++) { 
+     var random = (Math.floor(Math.random() * otherChar.length));
+      otherChar.textContent = otherChar[i];
+   }; 
+    return random;
+  };
   };
 
-  for(var i = 0; i < otherChar.length; i++) {
-    var random = (Math.floor(Math.random() * otherChar.length));
-    otherChar.textContent = otherChar[i];
- }; 
- 
 };
 
 // Add event listener to generate button
