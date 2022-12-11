96bcc1c2ef5477fecffd33830b17c31b50dc3dd2
Chito <Javier.martinez632@gmail.com>
2022-12-11 13:05:23 -0700

    updated var index

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");  
+var charLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var charUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var charNum = ["0","1","2","3","4","5","6","7","8","9"];
+var charSym = ["!","@","#","$","%","^","&","*","(",")","[","]","{","}"];
+var otherChar = [];
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -8,60 +15,59 @@ function writePassword() {
   
   passwordText.value = password;
 
+   console.log(password);
 
   function generatePassword() {
-    var charLower = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-    var charUpper = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-    var charNum = ["0,1,2,3,4,5,6,7,8,9"];
-    var charSym = ["!,@,#,$,%,^,&,*,(,),[,],{,}"];
-    var otherChar = [];
-    var passwordLength = prompt("Enter length of password: 8-128 characters");
-    var lengthPW = 8 < passwordLength && passwordLength < 128;
-    var option1 = confirm("Include lowercase?");
-    var option2 = confirm("Include uppercase?");
-    var option3 = confirm("Include numbers?");
-    var option4 = confirm("Include symbols?");
     
-console.log(lengthPW);
-
+    var passwordLength = window.prompt("Enter length of password: 8-128 characters");
+    var lengthPW = 8 <= passwordLength && passwordLength <= 128;
+    var option1 = window.confirm("Include lowercase?");
+    var option2 = window.confirm("Include uppercase?");
+    var option3 = window.confirm("Include numbers?");
+    var option4 = window.confirm("Include symbols?");
+    
+    
     if (!lengthPW) {
       alert("Must be 8-128 characters long");
-      generatePassword();
+      return;
     } 
 
     if(option1 && option2 && option3 && option4) {
-      otherChar.concat(charLower, charUpper, charNum, charSym);
-    } else if(option1 && option2 && option3) {
-      otherChar.concat(charLower, charUpper, charNum);
-    } else if(option1 && option2 && option4) {
-      otherChar.concat(charLower, charUpper, charSym);
-    } else if(option1 && option3 && option4) {
-      otherChar.concat(charLower, charNum, charSym);
-    } else if(option1 && option2) {
-      otherChar.concat(charLower, charUpper)
-    } else if(option1 && option3) {
-      otherChar.concat(charLower, charNum);
-    } else if(option1 && option4) {
-      otherChar.concat(charLower, charSym);
-    } else if(option2 && option3 && option4) {
-      otherChar.concat(charUpper, charNum, charSym);
-    } else if(option2 && option3) {
-      otherChar.concat(charUpper, charNum);
-    } else if(option2 && option4) {
-      otherChar.concat(charUpper, charSym);
+     otherChar = otherChar.concat(charLower, charUpper, charNum, charSym);
+     } else if(option1 && option2 && option3) {
+       otherChar = otherChar.concat(charLower, charUpper, charNum);
+     } else if(option1 && option2 && option4) {
+       otherChar = otherChar.concat(charLower, charUpper, charSym);
+     } else if(option1 && option3 && option4) {
+       otherChar = otherChar.concat(charLower, charNum, charSym);
+     } else if(option1 && option2) {
+       otherChar = otherChar.concat(charLower, charUpper)
+     } else if(option1 && option3) {
+       otherChar = otherChar.concat(charLower, charNum);
+     } else if(option1 && option4) {
+       otherChar = otherChar.concat(charLower, charSym);
+     } else if(option2 && option3 && option4) {
+       otherChar = otherChar.concat(charUpper, charNum, charSym);
+     } else if(option2 && option3) {
+       otherChar = otherChar.concat(charUpper, charNum);
+     } else if(option2 && option4) {
+      otherChar = otherChar.concat(charUpper, charSym);
     } else if(option3 && option4) {
-      otherChar.concat(charNum, charSym);
-    } 
-//     for(var i = 0; i < otherChar.concat().length; i++) { 
-//      var random = (Math.floor(Math.random() * otherChar.length));
-//       otherChar.textContent = otherChar[i];
-//    }; 
-//     return random;
-//   };
-//   };
+      otherChar = otherChar.concat(charNum, charSym);
+    } else {
+      return;
+    };
 
- };
-}; 
+    var index = Math.floor(Math.random() * otherChar.length);
+    var choicePW = otherChar[index];    
+
+    if(choicePW) {
+      window.alert("password generated successful");
+    };
+
+    };
+    
+   };
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
